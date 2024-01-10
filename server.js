25c51ec3b19f94d22ac0e71f59a958fe31d77c3d
David Sil <davidsil.27092022@gmail.com>
2024-01-10 21:33:01 +0700

    update server

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World, DucMinhUAT!');
+    res.end('Hello World, I am the men!');
 }).listen(3000);
